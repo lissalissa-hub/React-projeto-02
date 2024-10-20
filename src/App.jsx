949fac827ab8557d2fac996210533d7148cfde7c
lissalissa-hub <melissadias748@gmail.com>
2024-10-20 18:04:47 -0300

    Atividade02

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Textinho02 from './components/Textinho02'
+import './App.css'
+
+function App() {
+  return(
+    <>
+    <Textinho02 nome=" Obama"/>
+    <Textinho02 nome=" Lissa-Lissa"/>
+
+    </>
+   
+    
+  )
+  
+}
+
+export default App
---
+++ src/components/Textinho02.jsx
@@ -0,0 +1,5 @@
+function Textinho02(props){
+    return <h1>Olá,{props.nome}! </h1> 
+
+    }
+    export default Textinho02 
